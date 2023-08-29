6662e53a0063ee47e84bcddf9dc13c27d9f86486
Kyle Sudu <kyle.sudu@fleetcor.com>
2023-08-28 22:13:49 -0400

    Hi

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 fun main(args: Array<String>) {
     println("Hello These Nuts!!")
+    println("Verified commit, yo")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
