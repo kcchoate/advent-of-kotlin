e64e037b11e594165d5db327422ad4350409962b
Kyle S <ksudu94@gmail.com>
2023-08-28 22:16:01 -0400

    Lets try again

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     println("Hello These Nuts!!")
-    println("Verified commit, yo")
+    println("Antoher verified commit, yo")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
