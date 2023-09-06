8eb6ec3c674bfa06b0ceb20eb4c38fead9683db5
Joe Nace <jwnace@gmail.com>
2023-09-05 22:00:45 -0400

    Add function for counting depth increases, and add tests around various changes in depth

--- src/main/kotlin/day01/Solution.kt
+++ src/main/kotlin/day01/Solution.kt
@@ -3,9 +3,14 @@ package day01
 import java.io.File
 
 class Solution {
-    val input = File("input.txt").readLines().map { it.toInt() }
 
-    fun getNumberOfDepthIncreases(): Int {
-        return 0
+    fun getNumberOfDepthIncreases(depths: List<Int>): Int {
+        var total = 0;
+        for (i in 0 until depths.size - 1) {
+            if (depths[i] < depths[i + 1]) {
+                total++;
+            }
+        }
+        return total;
     }
 }
--- src/test/kotlin/day01/SolutionTests.kt
+++ src/test/kotlin/day01/SolutionTests.kt
@@ -7,19 +7,46 @@ class SolutionTests {
     private val sut = Solution()
 
     @Test
-    fun exampleInput_returns7() {
-        val input = """199
-200
-208
-210
-200
-207
-240
-269
-260
-263"""
+    fun singleIncrement_returns1() {
+        val input = listOf(199, 200);
 
-        val result = sut.getNumberOfDepthIncreases();
+        val result = sut.getNumberOfDepthIncreases(input);
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    fun singleDecrement_returns0() {
+        val input = listOf(200, 199);
+
+        val result = sut.getNumberOfDepthIncreases(input);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    fun equalDepths_returns0() {
+        val input = listOf(200, 200);
+
+        val result = sut.getNumberOfDepthIncreases(input);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    fun emptyInput_returns0() {
+        val input = emptyList<Int>();
+
+        val result = sut.getNumberOfDepthIncreases(input);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    fun exampleInput_returns7() {
+        val input = listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263);
+
+        val result = sut.getNumberOfDepthIncreases(input);
 
         assertEquals(7, result);
     }
