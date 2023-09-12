33cc9aa627913c3c266f4e2d98cc53a01026cc74
Kendrick Choate <kendrickchoate@gmail.com>
2023-09-11 20:38:22 -0500

    Implement part 2.

--- src/main/kotlin/day01/Solution.kt
+++ src/main/kotlin/day01/Solution.kt
@@ -2,6 +2,7 @@ package day01
 
 import java.io.File
 
+// https://adventofcode.com/2021/day/1
 class Solution {
 
     fun getNumberOfDepthIncreases(depths: List<Int>): Int {
@@ -14,8 +15,23 @@ class Solution {
         return total;
     }
 
+    fun getNumberOfWindowDepthIncreases(depths: List<Int>): Int {
+        var total = 0;
+        for (i in 0 until depths.size - 3) {
+            if (depths[i] < depths[i + 3]) {
+                total++;
+            }
+        }
+        return total;
+    }
+
     fun part1(): Int {
         val input = File("src/main/kotlin/day01/input.txt").readLines().map { it.toInt() }
         return getNumberOfDepthIncreases(input);
+    }
+
+    fun part2(): Int {
+        val input = File("src/main/kotlin/day01/input.txt").readLines().map { it.toInt() }
+        return getNumberOfWindowDepthIncreases(input);
     }
 }
--- src/test/kotlin/day01/SolutionTests.kt
+++ src/test/kotlin/day01/SolutionTests.kt
@@ -1,6 +1,10 @@
 package day01
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 import kotlin.test.assertEquals
 
 class SolutionTests {
@@ -56,5 +60,69 @@ class SolutionTests {
         val result = sut.part1();
 
         assertEquals(1624, result);
+    }
+
+    @Test
+    fun `part2 when first window is less than second window then we return 1`() {
+        val input = listOf(1, 2, 3, 4);
+
+        val result = sut.getNumberOfWindowDepthIncreases(input);
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    fun `part2 when first window is equal to second window then we return 0`() {
+        val input = listOf(2, 2, 2, 2);
+
+        val result = sut.getNumberOfWindowDepthIncreases(input);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    fun `part2 when first window is greater than second window then we return 0`() {
+        val input = listOf(4, 3, 2, 1);
+
+        val result = sut.getNumberOfWindowDepthIncreases(input);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    fun `part2 when input is empty then we return 0`() {
+        val input = emptyList<Int>();
+
+        val result = sut.getNumberOfWindowDepthIncreases(input);
+
+        assertEquals(0, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("tooShortLists")
+    fun `part2 when input is too short then we return 0`() {
+        val input = listOf(1);
+
+        val result = sut.getNumberOfWindowDepthIncreases(input);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    fun `part 2 returns correct value`() {
+        val result = sut.part2();
+
+        assertEquals(1653, result);
+    }
+
+    companion object {
+        @JvmStatic
+        private fun tooShortLists(): Stream<List<Int>> {
+            return Stream.of(
+                listOf(1),
+                listOf(1, 2),
+                listOf(1, 2, 3)
+            );
+        }
     }
 }
