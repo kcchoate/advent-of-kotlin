f895a74a789d54379e3eaec531f3c6b2c386ff56
Kendrick Choate <kendrickchoate@gmail.com>
2023-11-02 21:09:59 -0500

    Solve first part.

--- src/main/kotlin/day04/Solution.kt
+++ src/main/kotlin/day04/Solution.kt
@@ -1,13 +1,16 @@
 package day04
 
+import day04.models.BingoGame
 import java.io.File
 
 class Solution {
-    val inputs = File("src/main/kotlin/day04/input.txt")
-        .readText()
-        .split("\n\n")
+    val input = File("src/main/kotlin/day04/input.txt").readText()
 
     fun solve(): Int {
-        return 0
+        val game = BingoGame(input)
+        val lastNumber = game.play()
+        val winner = game.getWinner().get()
+
+        return winner.getScore(lastNumber)
     }
 }
